0cd43ebffae5ff004e976f49e824492dbd3e8faa
RinGoku <skyta80105@gmail.com>
2023-07-06 21:17:51 +0900

    fix

--- app/.svelte-kit/generated/client/nodes/7.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/app/sverdle/how-to-play/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/app/sverdle/how-to-play/+page.svelte";
