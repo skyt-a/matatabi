e790b650880df9d1e7b922c7c27d1eb81414ca3b
RinGoku <skyta80105@gmail.com>
2023-07-07 14:31:44 +0900

    fix

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,13 +1,12 @@
-import { decodeToken } from '$lib/server/firebase';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const token = event.cookies.get('token') || '';
-	const decodedToken = await decodeToken(token);
-	if (decodedToken) {
-		const { uid, name, email, picture } = decodedToken;
-		event.locals.userSession = { uid, name, email, picture };
-	}
+	// const token = event.cookies.get('token') || '';
+	// const decodedToken = await decodeToken(token);
+	// if (decodedToken) {
+	// 	const { uid, name, email, picture } = decodedToken;
+	// 	event.locals.userSession = { uid, name, email, picture };
+	// }
 
 	const response = await resolve(event);
 
